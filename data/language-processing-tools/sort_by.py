021b4b4a16a19736723c91c9362cbb3362217ee4
Jon Clucas <jon.clucas@childmind.org>
2017-02-03 12:08:43 -0500

    sort objects.json

--- data/language-processing-tools/sort_by.py
+++ data/language-processing-tools/sort_by.py
@@ -1,12 +1,35 @@
-var sort_by = function(field, reverse, primer){
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+sort_by.py
 
-   var key = primer ? 
-       function(x) {return primer(x[field])} : 
-       function(x) {return x[field]};
+A function to sort objects.json and save the sorted object file for easier
+manual editing of said file.
 
-   reverse = !reverse ? 1 : -1;
+Author:
+    –Jon Clucas, 2017
+    
+   © 2016, Child Mind Institute, The MIT License
+"""
+import json, operator
 
-   return function (a, b) {
-       return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
-     } 
-}
+def sort_oj():
+    """
+    A function to sort objects.json and save the sorted file.
+    """
+    sort_keys = {"method" : 1,
+                 "software" : 2,
+                 "paper" : 3,
+                 "protocol": 4,
+                 "database": 5,
+                 "domain" : 6}
+    with open("objects.json", "r") as oj:
+        data = json.load(oj)
+    new_json = sorted(data, key=operator.itemgetter('name'))
+    data = sorted(new_json, key=lambda x: sort_keys[x['type']])
+    with open('objects.json', 'w') as f:
+        json.dump(data, f, sort_keys=True, indent=4, separators=(',', ':'))
+
+# ============================================================================
+if __name__ == '__main__':
+    sort_oj()
